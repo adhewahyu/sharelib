17eabb60a108146082e0fdf21e3c32b449dede06
adhewahyu <51067003+adhewahyu@users.noreply.github.com>
2025-02-05 18:42:54 +0700

    remove iml

--- src/main/java/com/dan/sharelib/controller/BaseController.java
+++ src/main/java/com/dan/sharelib/controller/BaseController.java
@@ -1,6 +1,5 @@
 package com.dan.sharelib.controller;
 
-
 import com.dan.sharelib.enums.Message;
 import com.dan.sharelib.model.response.CommonResponse;
 import com.dan.sharelib.model.response.ValidationResponse;
@@ -8,12 +7,12 @@ import reactor.core.publisher.Mono;
 
 public class BaseController {
 
-    public Mono<CommonResponse> getCommonResponse(Mono<ValidationResponse> validationResponseMono) {
-        return validationResponseMono.flatMap(validationResponse -> Mono.just(CommonResponse.builder()
-                .data(null)
-                .message(validationResponse.getResult() ? Message.MSG_OK.getMsg() : Message.MSG_NOK.getMsg())
-                .result(validationResponse.getResult())
-                .build()));
-    }
+//    public Mono<CommonResponse> getCommonResponse(Mono<ValidationResponse> validationResponseMono) {
+//        return validationResponseMono.flatMap(validationResponse -> Mono.just(CommonResponse.builder()
+//                .data(null)
+//                .message(validationResponse.getResult() ? Message.MSG_OK.getMsg() : Message.MSG_NOK.getMsg())
+//                .result(validationResponse.getResult())
+//                .build()));
+//    }
 
 }
