0f017f65c95073ec3598182c9270127c354ba18d
adhewahyu <51067003+adhewahyu@users.noreply.github.com>
2025-02-05 20:39:56 +0700

    update base entity

--- src/main/java/com/dan/sharelib/model/entity/BaseApprovalEntity.java
+++ src/main/java/com/dan/sharelib/model/entity/BaseApprovalEntity.java
@@ -2,6 +2,7 @@ package com.dan.sharelib.model.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.relational.core.mapping.Column;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -10,11 +11,22 @@ import java.util.Date;
 @Getter
 public class BaseApprovalEntity implements Serializable {
 
+    @Column(value = "created_by")
     private String createdBy;
+
+    @Column(value = "created_date")
     private Date createdDate;
+
+    @Column(value = "updated_by")
     private String updatedBy;
+
+    @Column(value = "updated_date")
     private Date updatedDate;
+
+    @Column(value = "approved_by")
     private String approvedBy;
+
+    @Column(value = "approved_date")
     private Date approvedDate;
 
 }
--- src/main/java/com/dan/sharelib/model/entity/BaseEntity.java
+++ src/main/java/com/dan/sharelib/model/entity/BaseEntity.java
@@ -2,6 +2,7 @@ package com.dan.sharelib.model.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.relational.core.mapping.Column;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -10,9 +11,16 @@ import java.util.Date;
 @Getter
 public class BaseEntity implements Serializable {
 
+    @Column(value = "created_by")
     private String createdBy;
+
+    @Column(value = "created_date")
     private Date createdDate;
+
+    @Column(value = "updated_by")
     private String updatedBy;
+
+    @Column(value = "updated_date")
     private Date updatedDate;
 
 }
