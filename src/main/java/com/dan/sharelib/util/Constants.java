51bdeea15cdb3186b24da3c409eed5292f776397
adhewahyu <51067003+adhewahyu@users.noreply.github.com>
2025-02-05 21:19:37 +0700

    Update Constants.java

--- src/main/java/com/dan/sharelib/util/Constants.java
+++ src/main/java/com/dan/sharelib/util/Constants.java
@@ -2,6 +2,19 @@ package com.dan.sharelib.util;
 
 public class Constants {
 
+    public Constants(){}
+
     public static final String SYSTEM = "SYSTEM";
+
+    public static final String HTTP_CODE_200 = "200";
+    public static final String HTTP_CODE_400 = "400";
+    public static final String HTTP_CODE_404 = "404";
+    public static final String HTTP_CODE_500 = "500";
+
+    public static final String HTTP_DESC_200 = "OK";
+    public static final String HTTP_DESC_400 = "Bad Request";
+    public static final String HTTP_DESC_404 = "Not Found";
+    public static final String HTTP_DESC_500 = "Internal Server Error";
+
 
 }
